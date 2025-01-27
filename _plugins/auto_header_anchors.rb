be6996275a2e95da3cff1dfd2b3c28fd6565fecb
Owen Stephens <owen@owenstephens.co.uk>
2025-01-27 22:33:12 +0000

    Add header anchors everywhere

--- _plugins/auto_header_anchors.rb
+++ _plugins/auto_header_anchors.rb
@@ -1,17 +1,25 @@
 require "nokogiri"
 
-Jekyll::Hooks.register :documents, :post_render do |doc|
-  next unless doc.output_ext == ".html"
+def add_header_anchors(doc)
+  return unless doc.output_ext == ".html"
 
   html = Nokogiri::HTML(doc.output)
 
-  next unless (post_contents = html.at_css("#post-contents"))
+  return unless (container = html.at_css(".container"))
 
   header_elements_search = (1..6).map { |i| "h#{i}" }.join(",")
 
-  post_contents.search(header_elements_search).each do |h|
+  container.search(header_elements_search).each do |h|
     h.add_child(%(<a class="header_link" href="##{h["id"]}">🔗</a>))
   end
 
   doc.output = html.to_s
+end
+
+Jekyll::Hooks.register :pages, :post_render do |doc|
+  add_header_anchors(doc)
+end
+
+Jekyll::Hooks.register :documents, :post_render do |doc|
+  add_header_anchors(doc)
 end
