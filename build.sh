f6995b3cd34c01837c22cba78e1d808c42017070
Owen Stephens <owen@owenstephens.co.uk>
2025-01-29 22:31:20 +0000

    Commit build using GHA bot details

--- build.sh
+++ build.sh
@@ -11,4 +11,7 @@ cp -a _site/. master
 cd master
 git add .
 
-git commit -m "Build $(date -Iseconds)"
+# https://github.com/orgs/community/discussions/26560#discussioncomment-3252339
+git -c user.name='github-actions[bot]' \
+    -c user.email='41898282+github-actions[bot]@users.noreply.github.com' \
+    commit -m "Build $(date -Iseconds)"
